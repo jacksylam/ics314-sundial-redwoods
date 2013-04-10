edc4acc964e3cc7b92be1d8eb3914e29c7fdf4b8
Jack <catinacan@gmail.com>
2013-04-09 19:09:48 -1000

    More updates!

--- Sundial/src/sundial/SundialCalculation.java
+++ Sundial/src/sundial/SundialCalculation.java
@@ -15,6 +15,24 @@ public class SundialCalculation {
 	}
 	
 	public double getGnomeAngle(){
+		return latitude;
+	}
+	
+	public double calculateHourline(int hour){
+		double timeMeasruredFromNoonInDegrees;
+		double angleHourLine;
+		if(hour > 12){
+			int tempHour = hour - 12;
+			timeMeasruredFromNoonInDegrees = tempHour * 15;
+		}
+		else{
+			int tempHour = 12 - hour;
+			timeMeasruredFromNoonInDegrees = tempHour * 15;
+		}
+		
+		
+		angleHourLine = Math.atan(Math.sin(latitude)/Math.tan(timeMeasruredFromNoonInDegrees));
+		
 		return 0;
 	}
 	
@@ -24,7 +42,13 @@ public class SundialCalculation {
 	
 	private double calculateEOT(){
 		int dayOfYear = date.get(Calendar.DAY_OF_YEAR);
-		return 0;
+		double b = ((dayOfYear-81)/365)*360;
+		double e = 9.87*Math.sin(2*b) - 7.53*Math.cos(b) - 1.5*Math.sin(b);
+		return e;
+	}
+	
+	private boolean sanitizeInput(){
+		return true;
 	}
 
 }
