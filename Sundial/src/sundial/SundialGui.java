bb5d771c346f681a54ee30d8e2f9d0b85e4e50fc
Tirgardoon <Tirgardoon@gmail.com>
2013-04-30 06:57:07 +0000

    fixed spelling of latitude lol

--- Sundial/src/sundial/SundialGui.java
+++ Sundial/src/sundial/SundialGui.java
@@ -13,13 +13,13 @@ import javax.swing.*;
 public class SundialGui extends Applet implements ActionListener {
 	
 	protected Label title;
-	protected Label lattitudeLabel;
+	protected Label latitudeLabel;
 	protected Label longitudeLabel;
 	protected Label dateLabel;
 	protected Label info;
 	protected Label messageLabel;
 	
-	protected TextField lattitude;
+	protected TextField latitude;
 	protected TextField longitude;
 	
 	protected Checkbox daylightSavings;
@@ -39,8 +39,8 @@ public class SundialGui extends Applet implements ActionListener {
 		title = new Label("Sundial - Redwoods");
 		title.setFont(new Font("Arial",Font.BOLD, 20));
 		
-		lattitudeLabel = new Label("Lattitude: ");
-		lattitudeLabel.setFont(new Font("Arial",Font.PLAIN, 18));
+		latitudeLabel = new Label("Latitude: ");
+		latitudeLabel.setFont(new Font("Arial",Font.PLAIN, 18));
 		
 		longitudeLabel = new Label("Longitude: ");
 		longitudeLabel.setFont(new Font("Arial",Font.PLAIN, 18));
@@ -58,7 +58,7 @@ public class SundialGui extends Applet implements ActionListener {
 		daylightSavings = new Checkbox("Daylight Savings",false);
 		
 			
-		lattitude = new TextField("Lattitude");
+		latitude = new TextField("Latitude");
 		longitude = new TextField("Longitude");
 		month = new TextField("MM");
 		day = new TextField("DD");
@@ -67,8 +67,8 @@ public class SundialGui extends Applet implements ActionListener {
 		
 		title.setBounds(120,10,1000,20);
 		
-		lattitudeLabel.setBounds(90,100,90,20);
-		lattitude.setBounds(190,100,140,20);
+		latitudeLabel.setBounds(95,100,90,20);
+		latitude.setBounds(190,100,140,20);
 		
 		longitudeLabel.setBounds(80,140,90,20);
 		longitude.setBounds(190,140,140,20);
@@ -91,8 +91,8 @@ public class SundialGui extends Applet implements ActionListener {
 		
 		
 		add(title);
-		add(lattitudeLabel);
-		add(lattitude);
+		add(latitudeLabel);
+		add(latitude);
 		add(longitudeLabel);
 		add(longitude);
 		add(dateLabel);
@@ -113,7 +113,7 @@ public class SundialGui extends Applet implements ActionListener {
 	
 	public void actionPerformed(ActionEvent event){
 		
-		Double lattitudeValue;
+		Double latitudeValue;
 		Double longitudeValue;
 		Calendar calendar; 
 		Boolean dSavingsState;
@@ -128,8 +128,8 @@ public class SundialGui extends Applet implements ActionListener {
 		
 		if(event.getSource() == calculate) {
 			
-			//checking lattitude input
-			input = lattitude.getText().trim();
+			//checking latitude input
+			input = latitude.getText().trim();
 			try {
 				value = Double.parseDouble(input);
 				double absValue = Math.abs(value);
@@ -139,13 +139,13 @@ public class SundialGui extends Applet implements ActionListener {
 					this.messageLabel.setForeground(Color.red);
 					return;
 				}else {
-					lattitudeValue = value;
+					latitudeValue = value;
 					this.messageLabel.setText("Program running.....");
 					this.messageLabel.setForeground(Color.black);
 				}
 			}catch (NumberFormatException nfe) {
 				this.messageLabel.setText("ERROR: Please enter a numerical value" + 
-					" for \"lattitude\"");
+					" for \"latitude\"");
 				this.messageLabel.setForeground(Color.red);
 				return;
 			}
@@ -250,7 +250,7 @@ public class SundialGui extends Applet implements ActionListener {
 			Date temp = new Date();
 			Calendar temp2 = calendar;
 			temp2.setTime(temp);
-			SundialCalculation test = new SundialCalculation(lattitudeValue, longitudeValue, temp2);
+			SundialCalculation test = new SundialCalculation(latitudeValue, longitudeValue, temp2);
 			double[] temp3 = test.getModifiedHourAngles();
 //			for(int i = 0; i<temp3.length; i++){
 //				System.out.println(Math.toDegrees(temp3[i]));
